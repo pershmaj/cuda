902ee005857e38f7a838db827ee6163c673732ec
Your Name <you@example.com>
2021-11-18 10:16:32 +0000

    class4

---
+++ class4/build/CMakeFiles/ImageBlur.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageBlur.dir/main.cu.o"
+  "CMakeFiles/ImageBlur.dir/utils/pngio.cpp.o"
+  "ImageBlur"
+  "ImageBlur.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/ImageBlur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
